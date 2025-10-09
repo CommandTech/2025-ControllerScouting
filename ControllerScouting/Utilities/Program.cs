668e62d787e501a827bc8502e45394dfa0229d49
Logan Bachman <loganbachman812@gmail.com>
2025-10-09 10:35:56 -0400

    BaseScreen should be faster, removed SuspendLayout and ResumeLayout. CheckSQLExists doesn't print anymore

--- ControllerScouting/Utilities/Program.cs
+++ ControllerScouting/Utilities/Program.cs
@@ -37,13 +37,6 @@ namespace ControllerScouting.Utilities
             RegistryView registryView = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32;
             using RegistryKey hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView);
             RegistryKey instanceKey = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL", false);
-            if (instanceKey != null)
-            {
-                foreach (var instanceName in instanceKey.GetValueNames())
-                {
-                    Console.WriteLine(Environment.MachineName + @"\" + instanceName);
-                }
-            }
             Settings.Default.sqlExists = instanceKey != null;
 
             if (!Settings.Default.sqlExists)
